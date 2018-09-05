ec916c7260df0fdcab72ab7dc5e6585bb4cc09f3
VChernykh <33216308+VChernykh@users.noreply.github.com>
2018-09-06 02:23:08 +0500

    Update BSF(ширина).py

--- BSF(ширина).py
+++ BSF(ширина).py
@@ -10,12 +10,17 @@ def BFS(top):
     stack.append(top)
     visited[top] = 1
     while (len(stack)):
-        point = stack.pop(0)
+        point = stack.pop(0) #берем очередную вершину
         for i in range(1, len(matrix) + 1):
             if (visited[i - 1] == 0) and (matrix[point - 1][i - 1] == 1):
                 stack.append(i)
                 visited[i - 1] = visited[point] + 1
+
+                
+    for j in range(len(visited)): #проверка на случай несвязного графа
+        if (visited[j] == 0):
+            BFS(j)
     
             
-BFS(4)                #ввод номер стратовой вершины (произвольно)
+BFS(4)                      #ввод номер стратовой вершины (произвольно)
 print(min(visited))
