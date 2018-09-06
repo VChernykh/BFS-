8f3ee2f74ad767452e60fb1b73a303f51d88d6a0
VChernykh <33216308+VChernykh@users.noreply.github.com>
2018-09-06 14:45:42 +0500

    Update BSF(ширина).py

--- BSF(ширина).py
+++ BSF(ширина).py
@@ -1,34 +1,28 @@
 #Breadth First Search
 
-matrix = [[0,1,1,0,0],       #матрица смежности, задающая граф
+matrix = [[0,1,1,0,0],                #матрица смежности, задающая граф
           [1,0,0,1,0],
           [1,0,0,0,0],
           [0,1,0,0,1],
-          [0,0,0,1,0]]
-"""
-    Данный граф выглядит так:
-                    1
-                   / \
-                  2   3
-                 / 
-                4   
-               /
-              5 
-"""
-visited = [0] * len(matrix)  #список уровней вершин, 0 - для непосещенных вершин
-stack = []                   #создадим список (стэк) для очереди
+          [0,0,0,1,0]] 
+visited = [0] * len(matrix)           #список уровней вершин
+stack = []                            #создадим список (стэк) для очереди
 
-def BFS(top):
-    stack.append(top)        #добавим в очередь
-    visited[top] = 1         #1 - уже для посещенной вершины
+def BFS(start, end):                  #start - начальная вершина обхода, end - конечная
+    stack.append(start)               #добавим в очередь 
+    visited[start] = 1                
     while (len(stack)):
-        point = stack.pop(0) #берем очередную вершину из стэка
-        for i in range(1, len(matrix) + 1):        #положим в стэк непосещенные и связанные с данной вершины
+        point = stack.pop(0)          #берем очередную вершину
+        for i in range(1, len(matrix) + 1):     #положим в стэк непосещенные и связанные с данной вершины
             if (visited[i - 1] == 0) and (matrix[point - 1][i - 1] == 1):
                 stack.append(i)
                 visited[i - 1] = visited[point] + 1
 
                 
-    for j in range(len(visited)): #проверка на случай несвязного графа, переберем непосещенные вершины
-        if (visited[j] == 0):
-            BFS(j)
+    if(visited[end] == 0):
+        return "Not connect"
+    else:
+        return str(visited[end])
+    
+            
+print(BFS(4, 1))                      #ввод кратчайшего расстояния (в терминах количества рёбер)
